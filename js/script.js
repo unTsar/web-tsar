33d8f6fcf283ffb6ac85b88c6b27a7978710525c
unTsar <35622885+unTsar@users.noreply.github.com>
2018-05-21 19:27:04 +0400

    555
    555

--- js/script.js
+++
@@ -1,1 +0,0 @@
-var i=document.getElementById("header").offsetHeight;scroll1.onclick=function(){window.scrollTo(0,i)},scroll2.onclick=function(){window.scrollTo(0,10*i)},document.getElementById("f-req").onclick=function(){window.scrollTo(0,10*i)};
