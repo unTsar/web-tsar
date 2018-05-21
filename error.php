d846d9f3d0dcd8b8fdbbf01c5ecdff365f76e451
unTsar <35622885+unTsar@users.noreply.github.com>
2018-05-21 19:27:18 +0400

    666
    666

---
+++ error.php
@@ -0,0 +1,16 @@
+<?php
+    header('Refresh: 2; URL=http://tsar/');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        body{background-color: #004B7F; text-align: center; font-weight: bold;}
+        p{color: #BF7F30;}
+    </style>
+</head>
+<body>
+    <p>Пожалуйста, заполните необходимые поля! Вы будете перенаправлены на главную страницу через 2 секунды.</p>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,1 @@
+var i=document.getElementById("header").offsetHeight;scroll1.onclick=function(){window.scrollTo(0,i)},scroll2.onclick=function(){window.scrollTo(0,10*i)},document.getElementById("f-req").onclick=function(){window.scrollTo(0,10*i)};
