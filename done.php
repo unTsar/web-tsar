87bb483f7dfba06a90e6f9c81beef9aea132c143
unTsar <35622885+unTsar@users.noreply.github.com>
2018-05-22 12:09:23 +0400

    999
    999

---
+++ done.php
@@ -0,0 +1,1 @@
+<?php header('Refresh: 2; URL=http://tsar/');?><!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><style>body{background-color: #004B7F; text-align: center; font-weight: bold; font-size: 30px;}p{color: #BF7F30;}</style></head><body><p>Ваша заявка успешно отправлена! Вы будете перенаправлены на главную страницу через 2 секунды.</p></body></html>
